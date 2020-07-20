fede9a2c1cec84ebdc4bc769fad9f0176feb6afe
Nicholas Noll <nbnoll@eml.cc>
2020-07-20 15:39:53 -0700

    feat: log how many blocks are contained in extension zone

--- scripts/assay_junctions.py
+++ scripts/assay_junctions.py
@@ -22,6 +22,14 @@ sys.path.insert(0, os.path.abspath('.')) # gross hack
 from pangraph.tree  import Tree
 from pangraph.utils import parse_paf
 
+# ------------------------------------------------------------------------
+# globals
+
+EXTEND = 5000
+
+# ------------------------------------------------------------------------
+# helpers
+
 def log(*msg):
     print(*msg, file=sys.stderr)
 
@@ -36,6 +44,22 @@ def align_score(s1, s2):
     dna = lambda s: DNA(str(s))
     r   = align(dna(s1), dna(s2), env, False, True, True)
     return r[0]/max(len(s1), len(s2))
+
+def count_blocks_in(hits, hit, delta):
+    lb    = max(0, hit['start']-delta)
+    ub    = min(hit['len'], hit['end']+delta)
+    name  = hit['name']
+    start = hit['start']
+    end   = hit['end']
+
+    num = 0
+    for h in hits:
+        if h['qry']['name'] == name:
+            if lb <= h['qry']['start'] and h['qry']['start'] < start:
+                num += 1
+            elif end < h['qry']['start'] and h['qry']['start'] <= ub:
+                num += 1
+    return num
 
 def draw_figure(ticks, left, right):
     Dl = np.diag(1/l[:,0])@l
@@ -53,10 +77,16 @@ def draw_figure(ticks, left, right):
 
     fig.savefig("figs/alignment_score_extend_past_junction.png")
 
+# ------------------------------------------------------------------------
+# main point of entry
+
 def main(args):
-    ticks     = np.arange(0, 2000, 100)
+    ticks     = np.arange(0, EXTEND, 250)
     ljunctions, rjunctions = [], []
+    nblks_good = []
+    nblks_all  = []
     for d in args:
+        print(f"analyzing {d}")
         with open(f"{d}/guide.json") as fd:
             T = Tree.from_json(fd)
 
@@ -78,15 +108,27 @@ def main(args):
                 lscore = lambda qb, rb: align_score(qs[max(qb,0):ql[1]], rs[max(rb,0):rl[1]])
                 rscore = lambda qe, re: align_score(qs[qr[0]:min(qe, len(qs))], rs[rr[0]:min(re,len(rs))])
 
-                ljunctions.append([lscore(ql[0]-tick,rl[0]-tick) for tick in ticks])
-                if ljunctions[-1][-1] >= ljunctions[-1][0]:
-                    log(d, n.name)
-                    log(hit)
-
+                ljunctions.append([lscore(ql[0]-tick, rl[0]-tick) for tick in ticks])
                 rjunctions.append([rscore(qr[1]+tick, rr[1]+tick) for tick in ticks])
 
+                if ljunctions[-1][-1] >= .9*ljunctions[-1][0] or rjunctions[-1][-1] >= .9*rjunctions[-1][0]:
+                    # count number of blocks in extended zone
+                    nblks_good.append(count_blocks_in(hits, hit['qry'], EXTEND))
+                    nblks_good.append(count_blocks_in(hits, hit['ref'], EXTEND))
+                    # for manual inspection
+                    log(d, n.name)
+                    log(json.dumps(hit))
+
+                nblks_all.append(count_blocks_in(hits, hit['qry'], EXTEND))
+                nblks_all.append(count_blocks_in(hits, hit['ref'], EXTEND))
+
     ljunctions, rjunctions = np.array(ljunctions), np.array(rjunctions)
-    np.savez("data/junctions.npz", left=ljunctions, right=rjunctions, ticks=ticks)
+    nblks_good, nblks_all  = np.array(nblks_good), np.array(nblks_all)
+    np.savez("data/junctions.npz",
+            left=ljunctions, right=rjunctions, ticks=ticks,
+            blks_good=nblks_good, blks_all=nblks_all
+    )
+    draw_figure(ticks, ljunctions, rjunctions)
 
     return 0
 
