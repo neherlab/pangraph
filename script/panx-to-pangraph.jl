662e8d2e3952768506494bbe377a5513c99e0825
Nicholas Noll <nbnoll@eml.cc>
2021-12-21 12:56:38 -0800

    feat: scripts now generate panx comparison files
    Additionally, I updated the naming of each file to show the relationship
    between shell and julia scripts more explicitly. Benchmarking has been
    expanded upon to show scaling over a longer region.

--- script/panx-to-pangraph.jl
+++ script/panx-to-pangraph.jl
@@ -1,5 +1,7 @@
 module PanX
 
+using Printf
+using JSON, GZip
 using PanGraph
 
 struct Gene
@@ -32,20 +34,99 @@ function gene(line, name, database)
 end
 
 struct Genome
+    name     :: String
     sequence :: Array{UInt8,1}
     gene     :: Array{Gene,1}
 end
-Genome(sequence::Array{UInt8,1}) = Genome(sequence, Gene[])
 
-function main(path)
-    database = Set{String}[]
+Genome(name::String, sequence::Array{UInt8,1}) = Genome(name, sequence, Gene[])
+
+function database(path)
+    data = Set{String}[]
+    meta = String[]
+    re   = r"(N[A-Z]_[A-Z0-9]+)_"
+    sub  = s"\1|"
     open(path) do io
-        for (i,gene) in enumerate(eachline(path))
-            push!(database, Set(split(gene)))
+        genes = JSON.parse(io)
+        for (i,gene) in enumerate(genes)
+            loci = map(split(gene["locus"])) do g
+                replace(g, re => sub)
+            end
+            push!(data, Set(loci))
+            push!(meta, gene["msa"])
         end
     end
-    @show database
+    return (
+        loci=data,
+        meta=meta,
+        path=dirname(path),
+    )
+end
 
+function alignment(io::IO)
+    record = [ (name=rec.name, seq=rec.seq) for rec in PanGraph.Utility.read_fasta(io)]
+    return hcat(map(r->r.seq,record)...), map(record) do r
+        entry = split(r.name, "-")
+        return "$(entry[1])|$(entry[2])"
+    end
+end
+
+function pangraph(genomes, genes)
+    vertex = [
+        let
+            path = "$(genes.path)/geneCluster/$(base)_na_aln.fa.gz"
+            aln, isos = GZip.open(alignment, path)
+
+            ref = PanGraph.Utility.make_consensus(aln)
+            blk = PanGraph.Graphs.Block(ref)
+            iso = [ PanGraph.Graphs.Node(blk, true) for _ in isos ]
+
+            blk.uuid = @sprintf("Gene_%05d", i)
+            blk.gaps,
+            blk.mutate,
+            blk.delete,
+            blk.insert,
+            blk.sequence = PanGraph.Utility.alignment_alleles(ref, aln, iso)
+
+            (block=blk, node=Dict( name => node for (name,node) in zip(isos,iso)))
+        end for (i,(gene,base)) in enumerate(zip(genes.loci,genes.meta))
+    ]
+
+    path = [
+        let
+            nodes = [
+             let
+                 node = pop!(vertex[gene.id].node, askey(genome.name, gene.tag))
+                 node.strand = gene.strand
+                 node
+             end for gene in genome.gene
+            ]
+            breaks = [ x for gene in genome.gene for x in [gene.low, gene.high] ]
+            PanGraph.Graphs.Path(genome.name, nodes, 0, true, breaks)
+        end for genome in values(genomes)
+    ]
+
+    # remove any unpaired nodes we initially added
+    # delete empty blocks
+    index = Int[]
+    for (i,v) in enumerate(vertex)
+        for n in values(v.node)
+            pop!(v.block, n)
+        end
+
+        if length(v.block.mutate) == 0
+            push!(index, i)
+        end
+    end
+    deleteat!(vertex, index)
+
+    block = map(v->v.block, vertex)
+    graph = PanGraph.Graph(Dict(b.uuid => b for b in block), Dict(p.name => p for p in path) )
+
+    return graph
+end
+
+function main(database)
     genomes = Dict{String,Genome}()
     while !eof(stdin)
         line = readline(stdin)
@@ -61,13 +142,13 @@ function main(path)
             print(sbuf, line)
             @goto moreseq
         end
-        genome = Genome(take!(sbuf))
+        genome = Genome(name, take!(sbuf))
 
         # grab loci
         @label moregene
         line = readline(stdin)
         if line != "ENDGENE"
-            g = gene(line, name, database)
+            g = gene(line, name, database.loci)
             g !== nothing && push!(genome.gene, g)
             @goto moregene
         end
@@ -75,9 +156,7 @@ function main(path)
         genomes[name] = genome
     end
 
-    @show genomes
-
-    # TODO: turn into a pangraph
+    return pangraph(genomes, database)
 end
 
 # NOTE: just for debugging
@@ -88,8 +167,7 @@ function echo()
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
-    main(ARGS[1])
-    # echo()
+    database(ARGS[1]) |> main |> (G) -> PanGraph.Graphs.marshal(stdout, G; fmt=:json)
 end
 
 end
--- script/rules.mk
+++ script/rules.mk
@@ -1,19 +1,19 @@
-.PHONY: fig1 panX panX2
+.PHONY: fig1 panx
 
 panx-dir  := data/panx/kleb
-fig1-data := $(datadir)/alignment-compare.jld2
+accuracy  := $(datadir)/alignment-compare.jld2
 benchmark := $(datadir)/benchmark.txt
 
-# comparison to synthetic
-$(fig1-data): script/make-sequence.jl script/assay-alignment.jl
-	@echo "collecting data for figure 1...";\
-	script/make-comparison $^ $@
-
-# benchmark
+# benchmarking
 $(benchmark): script/benchmark
 	$^ > $@
 
-# comparison to panX
+# accuracy relative to simulated
+$(accuracy): script/make-sequence.jl script/make-accuracy.jl
+	@echo "collecting data for figure 1...";\
+	script/make-accuracy $^ $@
+
+# accuracy relative to panX gene boundaries
 panx-input-gb := $(wildcard $(panx-dir)/input_GenBank/*.gbk)
 panx-input-fa := $(patsubst $(panx-dir)/input_GenBank/%.gbk, $(panx-dir)/fa/%.fa, $(panx-input-gb))
 
@@ -21,16 +21,17 @@ $(panx-dir)/fa:
 	mkdir -p $@
 
 $(panx-dir)/fa/%.fa: $(panx-dir)/input_GenBank/%.gbk | $(panx-dir)/fa
+	@echo MAKE	$@;\
 	script/gbk-to-fa $* <$< >$@
 
 $(panx-dir)/panx.json: $(panx-input-fa) $(panx-input-gb)
-	@echo making $@;\
-	script/panx-to-pangraph $(panx-dir)/allclusters_final.tsv $^
+	@echo MAKE	$@;\
+	script/panx-to-pangraph $(panx-dir)/vis/geneCluster.json $@ $^
 
 $(panx-dir)/pangraph.json: $(panx-input-fa)
-	JULIA_NUM_THREADS=8 pangraph build --circular -m 0 -b 50 -l 50 $^ 2>/dev/null 1>$@
+	@echo MAKE	$@;\
+	JULIA_NUM_THREADS=8 pangraph build --circular -m 0 -b 0 -l 50 $^ 1>$@
 
-panX2: $(panx-dir)/panx.json
-panX: $(panx-dir)/pangraph.json
+panx: $(panx-dir)/pangraph.json $(panx-dir)/panx.json
 
-fig1: $(fig1-data)
+fig1: $(accuracy)
