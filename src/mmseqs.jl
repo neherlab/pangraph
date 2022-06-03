8fd5b2a42510b925efbe24328b9766447d2ef49c
Marco Molari <mmolari91@gmail.com>
2022-06-03 12:00:19 +0200

    fix: tentative fix for multithreading error
    using one call (`mmseqs easy-search`) instead of multiple for
    aligning query and reference with `mmseqs`

--- src/mmseqs.jl
+++ src/mmseqs.jl
@@ -6,10 +6,6 @@ import ..PanGraph.Graphs.Shell: execute
 
 export align
 
-function log(msg...)
-    println(stderr, msg...)
-    flush(stderr)
-end
 
 """
     align(ref::PanContigs, qry::PanContigs)
@@ -20,14 +16,10 @@ function align(ref::PanContigs, qry::PanContigs)
 
     hits = mktempdir() do dir
         # hits = let dir = mktempdir(; cleanup = false)
-        # log("starting to aling ref and qry in $dir")
 
-        qrydb, refdb = "$dir/qry", "$dir/ref"
-        if ref == qry
-            refdb = qrydb
-        end
+        qryfa, reffa = "$dir/qry.fa", "$dir/ref.fa"
 
-        open("$qrydb.fa", "w") do io
+        open("$qryfa", "w") do io
             for (name, seq) in zip(qry.name, qry.sequence)
                 if length(seq) ≥ 95
                     write_fasta(io, name, seq)
@@ -35,63 +27,29 @@ function align(ref::PanContigs, qry::PanContigs)
             end
         end
 
-        # log("mmseqs createdb")
-        run(
-            pipeline(
-                `mmseqs createdb $qrydb.fa $qrydb`,
-                stdout = "$dir/out_createdb_qry.log",
-                stderr = "$dir/err_createdb_qry.log",
-            ),
-        )
-
         if ref != qry
-
-            open("$refdb.fa", "w") do io
+            open("$reffa", "w") do io
                 for (name, seq) in zip(ref.name, ref.sequence)
                     if length(seq) ≥ 95
                         write_fasta(io, name, seq)
                     end
                 end
             end
-
-            run(
-                pipeline(
-                    `mmseqs createdb $refdb.fa $refdb`,
-                    stdout = "$dir/out_createdb_ref.log",
-                    stderr = "$dir/err_createdb_ref.log",
-                ),
-            )
-
+        else
+            reffa = qryfa
         end
 
-        # log("mmseqs search")
         run(
-            pipeline(
-                `mmseqs search 
-                --threads 1 
-                -a 
-                --max-seq-len 10000
-                --search-type 3
-                $qrydb $refdb $dir/res $dir/tmp`,
-                stdout = "$dir/out_search.log",
-                stderr = "$dir/err_search.log",
-            ),
+            `mmseqs easy-search
+            $qryfa $reffa $dir/res.paf $dir/tmp
+            --threads 1
+            --max-seq-len 10000
+            -a
+            --search-type 3
+            --format-output query,qlen,qstart,qend,empty,target,tlen,tstart,tend,nident,alnlen,bits,cigar,fident,raw`,
+            wait = true,
         )
 
-        # log("mmseqs convertalis")
-        run(
-            pipeline(
-                `mmseqs convertalis
-                --threads 1 
-                --search-type 3
-                $qrydb $refdb $dir/res $dir/res.paf
-                --format-output query,qlen,qstart,qend,empty,target,tlen,tstart,tend,nident,alnlen,bits,cigar,fident,raw`,
-                stdout = "$dir/out_convert.log",
-                stderr = "$dir/err_convert.log",
-            ),
-        )
-
-        # log("parse paf file")
         open(read_mmseqs2, "$dir/res.paf")
     end
 
