b02f7e61ab4887d6171c97d7397f7fff6527187c
Marco Molari <molari0000@login20.cluster.bc2.ch>
2022-06-18 16:30:05 +0200

    feat: changes to reproduce panx plots

--- script/rules.mk
+++ script/rules.mk
@@ -27,32 +27,32 @@ figs/paper-accuracy-%.png: $(accuracy)-%.jld2
 
 # accuracy relative to panX gene boundaries
 define PANX
-$(eval input-gb := $(wildcard data/panx/$(1)/input_GenBank/*.gbk))
-$(eval input-fa := $(patsubst data/panx/$(1)/input_GenBank/%.gbk, data/panx/$(1)/fa/%.fa, $(input-gb)))
-data/panx/$(1)/fa:
+$(eval input-gb := $(wildcard script/panx_data/$(1)/input_GenBank/*.gbk))
+$(eval input-fa := $(patsubst script/panx_data/$(1)/input_GenBank/%.gbk, script/panx_data/$(1)/fa/%.fa, $(input-gb)))
+script/panx_data/$(1)/fa:
 	mkdir -p $$@
 
-data/panx/$(1)/fa/%.fa: data/panx/$(1)/input_GenBank/%.gbk | data/panx/$(1)/fa
+script/panx_data/$(1)/fa/%.fa: script/panx_data/$(1)/input_GenBank/%.gbk | script/panx_data/$(1)/fa
 	@echo "MAKE	$$@";\
 	script/gbk-to-fa $$* <$$< >$$@
 
-data/panx/$(1)/panx.json: $(input-fa) $(input-gb)
+script/panx_data/$(1)/panx.json: $(input-fa) $(input-gb)
 	@echo "MAKE	$$@";\
-	script/panx-to-pangraph data/panx/$(1)/vis/geneCluster.json $$@ $$^
+	script/panx-to-pangraph script/panx_data/$(1)/vis/geneCluster.json $$@ $$^
 
-data/panx/$(1)/pangraph.json: $(input-fa)
+script/panx_data/$(1)/pangraph.json: $(input-fa)
 	@echo "MAKE	$$@";\
 	JULIA_NUM_THREADS=8 pangraph build --circular --upper-case --max-self-map 50 $$^ 1>$$@
 
-$(eval panx-targets += data/panx/$(1)/pangraph.json data/panx/$(1)/panx.json)
+$(eval panx-targets += script/panx_data/$(1)/pangraph.json script/panx_data/$(1)/panx.json)
 endef
 
 panx-targets =
-$(eval $(call PANX,kleb))
-$(eval $(call PANX,helio))
-$(eval $(call PANX,ecoli))
-$(eval $(call PANX,myco))
-$(eval $(call PANX,proc))
+$(eval $(call PANX,klebsiella_pneumoniae))
+$(eval $(call PANX,helicobacter_pylori))
+$(eval $(call PANX,escherichia_coli))
+$(eval $(call PANX,mycobacterium_tuberculosis))
+$(eval $(call PANX,prochlorococcus_marinus))
 
 figs/panx-compare.png: $(panx-targets)
 	julia --project=script script/plot-panx-compare.jl $^
