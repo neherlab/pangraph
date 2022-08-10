5083db7e0e4f83621d4d3e3865786ebf3fc3997e
Marco Molari <mmolari91@gmail.com>
2022-08-10 12:07:34 +0200

    chore: added comments

--- script/workflow_scripts/paper_figs/projections_plot.py
+++ script/workflow_scripts/paper_figs/projections_plot.py
@@ -16,6 +16,7 @@ def parse_args():
 
 
 def extract_species(csv):
+    """Given the csv file name extracts and returns the formatted species name."""
     name = re.search(r"benchmark/[^/_]+_([^\.]+)\.full\.csv", csv).group(1)
     name = name.split("_")
     sp_name = name[0][0].upper() + ". " + name[1].capitalize()
@@ -23,6 +24,8 @@ def extract_species(csv):
 
 
 def load_dfs(csvs):
+    """Given the list of filenames loads the dataframes and returns a
+    dictionary {species -> df}."""
     data = {}
     for csv in csvs:
         species = extract_species(csv)
@@ -31,13 +34,15 @@ def load_dfs(csvs):
     return data
 
 
-def ax_boxplot(df, species, ax):
+def ax_boxplot(df, ax):
+    """Boxplot the relevant variables given the sub-dataframe and the ax."""
 
+    # unstack and reformat dataframe
     sdf = df.reset_index(drop=True).unstack().reset_index()
     sdf = sdf.drop(columns=["level_1"])
     sdf = sdf.rename(columns={"level_0": "kind", 0: "value"})
 
-    # box-plots
+    # perform box-plot
     sns.boxplot(ax=ax, data=sdf, x="value", y="kind", color="C0", width=0.6)
     ax.set_ylabel("")
     ax.set_xlabel("")
@@ -52,9 +57,10 @@ def ax_boxplot(df, species, ax):
     #     ax.text(M * 1.1, nc, f"{mean:.2} $\pm$ {std:.2}")
 
 
-def add_column_header(ax, header):
+def add_column_title(ax, title):
+    """Add title to column, given the first ax of the column"""
     ax.annotate(
-        header,
+        title,
         xy=(0.5, 1),
         xytext=(0, 5),
         xycoords="axes fraction",
@@ -65,26 +71,38 @@ def add_column_header(ax, header):
 
 
 def projection_plot(data, savename):
-    NS = len(data)  # number of species
 
+    # setup figure
+    NS = len(data)  # number of species
     fig, axs = plt.subplots(NS, 2, figsize=(8, 1.5 * NS), sharey=True, sharex="col")
 
+    # perform the two plots for each species
     for ns, species in enumerate(data):
-        df = data[species]
 
+        df = data[species]  # select species dataframe
+
+        # shared genome fraction
         ax = axs[ns, 0]
-        ax_boxplot(df["fraction of total genome length"], species, ax)
+        sdf = df["fraction of total genome length"]
+        ax_boxplot(sdf, ax)
+
+        # add species name
         ax.text(0, 0, f"{species}", weight="bold", size="small")
 
+        # shared segment sizes
         ax = axs[ns, 1]
-        ax_boxplot(df["average segment size (kbp)"], species, ax)
-        ax.set_xscale("log")
+        sdf = df["average segment size (kbp)"]
+        ax_boxplot(sdf, ax)
 
-    add_column_header(axs[0, 0], "fraction of total genome length")
-    add_column_header(axs[0, 1], "average segment size (kbp)")
+    # add titles
+    add_column_title(axs[0, 0], "fraction of total genome length")
+    add_column_title(axs[0, 1], "average segment size (kbp)")
 
+    # tweak second column ax
+    ax.set_xscale("log")
     axs[0, 1].set_xticks(10.0 ** np.arange(-3, 4))
 
+    # despine and save
     sns.despine(fig)
     plt.tight_layout()
     plt.savefig(savename)
