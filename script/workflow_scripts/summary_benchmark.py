dd47dbd345c353832c8b1f268c4a08f77c75c9ad
Marco Molari <mmolari91@gmail.com>
2022-08-10 15:13:27 +0200

    chore: order kernels in panx summary plot

--- script/workflow_scripts/summary_benchmark.py
+++ script/workflow_scripts/summary_benchmark.py
@@ -6,15 +6,15 @@ import seaborn as sns
 
 # patterns to capture from the txt file
 patterns = {
-    "species" : "species = ([\S]+)", # string
-    "kind" : "kind = ([\S]+)", # string
-    "n-isolates" : 'Command being timed: "(.*)"',
-    "mem" : "Maximum resident set size \(kbytes\): ([\d]+)", # int
-    "wall-time" : "Elapsed \(wall clock\) time \(h:mm:ss or m:ss\): ([\S]+)", # time
-    "usr-time" : "User time \(seconds\): ([\S]+)", # float
-    "sys-time" : "System time \(seconds\): ([\S]+)", # float
-    "cpu-percent" : "Percent of CPU this job got: ([\d]+)%", # int
-    "command" : 'Command being timed: "(.*)"', #str
+    "species": "species = ([\S]+)",  # string
+    "kind": "kind = ([\S]+)",  # string
+    "n-isolates": 'Command being timed: "(.*)"',
+    "mem": "Maximum resident set size \(kbytes\): ([\d]+)",  # int
+    "wall-time": "Elapsed \(wall clock\) time \(h:mm:ss or m:ss\): ([\S]+)",  # time
+    "usr-time": "User time \(seconds\): ([\S]+)",  # float
+    "sys-time": "System time \(seconds\): ([\S]+)",  # float
+    "cpu-percent": "Percent of CPU this job got: ([\d]+)%",  # int
+    "command": 'Command being timed: "(.*)"',  # str
 }
 
 # time in format hh:mm:ss.ss into seconds
@@ -28,15 +28,19 @@ def parsetime(x):
 
 # conversion functions to transform captured patterns into values
 convert = {
-    "species" : str,
-    "kind" : str,
-    "n-isolates" : lambda x : len([y for y in x.split(" ") if y.startswith("panx_data/")]),
-    "mem" : lambda x : int(x) / 1e6,
-    "wall-time" : parsetime,
-    "usr-time" : lambda x: float(x) / (3600),
-    "sys-time" : lambda x: float(x) / (3600),
-    "cpu-percent" : int,
-    "command" : lambda x : " ".join([y for y in x.split(" ") if not y.startswith("panx_data/")]),
+    "species": str,
+    "kind": str,
+    "n-isolates": lambda x: len(
+        [y for y in x.split(" ") if y.startswith("panx_data/")]
+    ),
+    "mem": lambda x: int(x) / 1e6,
+    "wall-time": parsetime,
+    "usr-time": lambda x: float(x) / (3600),
+    "sys-time": lambda x: float(x) / (3600),
+    "cpu-percent": int,
+    "command": lambda x: " ".join(
+        [y for y in x.split(" ") if not y.startswith("panx_data/")]
+    ),
 }
 
 # given a pattern extract the first occurrence from the file
@@ -46,6 +50,7 @@ def extract_pattern(m, lines):
         return ""
     else:
         return x[0]
+
 
 # extract all patterns from a list of files
 def extract_info(f_name):
@@ -59,34 +64,44 @@ def extract_info(f_name):
         captures[k] = convert[k](val)
     return captures
 
+
+def order_kinds(kinds):
+    k_inv = [k[::-1] for k in kinds]
+    k_ordered = [k[::-1] for k in sorted(k_inv)]
+    return k_ordered
+
+
 # plot with benchmark statistics
 def plot_benchmark(df):
 
     # modify a copy of the dataframe
     df = df.copy()
 
-    # from full species name to abbreviation 
+    # from full species name to abbreviation
     def short_name(x):
         y = x.split("_")
         y[0] = y[0][0]
         y = [str.capitalize(s) for s in y]
         return ". ".join(y)
+
     df["species"] = df["species"].apply(short_name)
 
     # order the species
     order = df.groupby("species")["n-isolates"].mean().sort_values().index.to_numpy()
     df["species"] = pd.Categorical(df["species"], categories=order, ordered=True)
+    order = order_kinds(list(df["kind"].unique()))
+    df["kind"] = pd.Categorical(df["kind"], categories=order, ordered=True)
 
     # create figure
     N = 4
-    fig, axs = plt.subplots(N,1, figsize=(8,N*4))
+    fig, axs = plt.subplots(N, 1, figsize=(8, N * 4))
 
     # plot n. isolates
     ax = axs[0]
     sns.barplot(x="species", y="n-isolates", data=df, ax=ax)
 
     # plot wall-time
-    ax=axs[1]
+    ax = axs[1]
     df["wall-time (h)"] = df["wall-time"] / 3600
     sns.barplot(x="species", hue="kind", y="wall-time (h)", data=df, ax=ax)
     ax.set_yscale("log")
@@ -107,14 +122,17 @@ def plot_benchmark(df):
     return fig, axs
 
 
-
 if __name__ == "__main__":
 
     # parse arguments
-    parser = argparse.ArgumentParser(description="creates a table with summary statistics from the benchmark data")
+    parser = argparse.ArgumentParser(
+        description="creates a table with summary statistics from the benchmark data"
+    )
     parser.add_argument("out_csv", help="name of the output csv table", type=str)
     parser.add_argument("out_pdf", help="name of the output plots", type=str)
-    parser.add_argument("benchmarks", nargs="*", help="list of input summary txt files", type=str)
+    parser.add_argument(
+        "benchmarks", nargs="*", help="list of input summary txt files", type=str
+    )
     args = parser.parse_args()
 
     # extract info from input files
