0668ab770800d5deba04dc7b048cba57efd90a71
ivan-aksamentov <ivan.aksamentov@gmail.com>
2024-02-22 12:14:16 +0100

    feat: apply edits to ref

--- packages/pangraph/src/pangraph/edits.rs
+++ packages/pangraph/src/pangraph/edits.rs
@@ -1,16 +1,19 @@
+use crate::utils::collections::insert_at_inplace;
+use eyre::Report;
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
-
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
-pub struct Edits {
-  subs: Vec<Sub>,
-  dels: Vec<Del>,
-  inss: Vec<Ins>,
-}
+use std::ops::Range;
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Sub {
   pos: usize,
   alt: u8,
+}
+
+impl Sub {
+  pub fn new(pos: usize, alt: u8) -> Self {
+    Self { pos, alt }
+  }
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
@@ -19,8 +22,136 @@ pub struct Del {
   len: usize,
 }
 
+impl Del {
+  pub fn new(pos: usize, len: usize) -> Self {
+    Self { pos, len }
+  }
+
+  pub fn end(&self) -> usize {
+    self.pos + self.len
+  }
+
+  pub fn range(&self) -> Range<usize> {
+    self.pos..self.end()
+  }
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Ins {
   pos: usize,
-  ins: String,
+  seq: String,
+}
+
+impl Ins {
+  pub fn new(pos: usize, seq: impl AsRef<str>) -> Self {
+    Self {
+      pos,
+      seq: seq.as_ref().to_owned(),
+    }
+  }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Edits {
+  subs: Vec<Sub>,
+  dels: Vec<Del>,
+  inss: Vec<Ins>,
+}
+
+impl Edits {
+  /// Apply the edits to the reference to obtain the query sequence
+  pub fn apply(&self, reff: impl AsRef<str>) -> Result<String, Report> {
+    let mut qry: Vec<u8> = reff.as_ref().to_owned().into();
+
+    for sub in &self.subs {
+      qry[sub.pos] = sub.alt;
+    }
+
+    for del in &self.dels {
+      qry.drain(del.range());
+    }
+
+    for ins in &self.inss {
+      insert_at_inplace(&mut qry, ins.pos, ins.seq.as_bytes());
+    }
+
+    let qry = String::from_utf8(qry)?;
+    Ok(qry)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use eyre::Report;
+  use pretty_assertions::assert_eq;
+  use rstest::rstest;
+
+  #[rstest]
+  fn test_edits_apply_simple_case() {
+    //                 1
+    //       01234567890
+    let r = "ACCTGGCTTT";
+    let q = "AGCCTGGTAT";
+
+    let inss = vec![Ins::new(1, "G")];
+    let dels = vec![Del::new(6, 1)];
+    let subs = vec![Sub::new(8, b'A')];
+    let edits = Edits { subs, dels, inss };
+
+    let actual = edits.apply(r).unwrap();
+    assert_eq!(q, actual);
+  }
+
+  #[rstest]
+  fn test_edits_apply_sub() {
+    //       0123456789
+    let r = "ACCTGGCTTT";
+    let q = "ACCAGGCTTT";
+    //          s
+
+    let edits = Edits {
+      subs: vec![Sub::new(3, b'A')],
+      dels: vec![],
+      inss: vec![],
+    };
+
+    let actual = edits.apply(r).unwrap();
+    assert_eq!(q, actual);
+  }
+
+  #[rstest]
+  fn test_edits_apply_del() {
+    //       0123456789
+    let r = "ACCTGGCTTT";
+    let q = "ACCGCTTT";
+    //          d
+
+    let edits = Edits {
+      subs: vec![],
+      dels: vec![Del::new(3, 2)],
+      inss: vec![],
+    };
+
+    let actual = edits.apply(r).unwrap();
+    assert_eq!(q, actual);
+  }
+
+  #[rstest]
+  fn test_edits_apply_ins() {
+    //
+    //       0123456789
+    let r = "ACCTGGCTTT";
+    let q = "ACCTACGGCTTT";
+    //           ii
+
+    let edits = Edits {
+      subs: vec![],
+      dels: vec![],
+      inss: vec![Ins::new(4, "AC")],
+    };
+
+    let actual = edits.apply(r).unwrap();
+    assert_eq!(q, actual);
+  }
 }
--- packages/pangraph/src/utils/collections.rs
+++ packages/pangraph/src/utils/collections.rs
@@ -25,3 +25,60 @@ pub fn remove_exactly_one<T>(mut elems: Vec<T>) -> Result<T, Report> {
     _ => make_error!("Expected exactly one element, but found: {}", elems.len()),
   }
 }
+
+/// Insert slice into vec at an index
+/// Taken from: https://internals.rust-lang.org/t/add-vec-insert-slice-at-to-insert-the-content-of-a-slice-at-an-arbitrary-index/11008
+pub fn insert_at_inplace<T: Clone>(vec: &mut Vec<T>, index: usize, slice: &[T]) {
+  let len = vec.len();
+  assert!(
+    index <= len,
+    "Attempted to insert outside of array boundaries: array size is {len}, index is {index}"
+  );
+  vec.reserve(slice.len());
+  let mut v = vec.split_off(index);
+  vec.extend_from_slice(slice);
+  vec.append(&mut v);
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use eyre::Report;
+  use pretty_assertions::assert_eq;
+  use rstest::rstest;
+
+  #[rstest]
+  fn test_insert_at_inplace_general_case() {
+    let mut vec = vec![1; 6];
+    let slice = [0; 2];
+    let index = 2;
+    insert_at_inplace(&mut vec, index, &slice);
+    assert_eq!(vec![1, 1, 0, 0, 1, 1, 1, 1], vec);
+  }
+
+  #[rstest]
+  fn test_insert_at_inplace_append() {
+    let mut vec = vec![1; 6];
+    let slice = [0; 2];
+    let index = 6;
+    insert_at_inplace(&mut vec, index, &slice);
+    assert_eq!(vec![1, 1, 1, 1, 1, 1, 0, 0], vec);
+  }
+
+  #[rstest]
+  fn test_insert_at_inplace_prepend() {
+    let mut vec = vec![1; 6];
+    let slice = [0; 2];
+    let index = 0;
+    insert_at_inplace(&mut vec, index, &slice);
+    assert_eq!(vec![0, 0, 1, 1, 1, 1, 1, 1], vec);
+  }
+
+  #[rstest]
+  #[should_panic(expected = "Attempted to insert outside of array boundaries: array size is 6, index is 10")]
+  fn test_insert_at_inplace_out_of_bounds() {
+    let mut vec = vec![1; 6];
+    let index = 10;
+    insert_at_inplace(&mut vec, index, &[]);
+  }
+}
