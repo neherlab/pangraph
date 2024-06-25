1b45c044968c79bd1c6d6abd4ecfb51874745c50
ivan-aksamentov <ivan.aksamentov@gmail.com>
2024-06-25 15:53:48 +0200

    chore: temporarily comment-out broken code
    Co-authored-by: Marco Molari <mmolari91@gmail.com>

--- packages/pangraph/src/commands/build/build_run.rs
+++ packages/pangraph/src/commands/build/build_run.rs
@@ -33,9 +33,9 @@ pub fn build_run(args: &PangraphBuildArgs) -> Result<(), Report> {
 }
 
 pub fn build(fastas: Vec<FastaRecord>, args: &PangraphBuildArgs) -> Result<Pangraph, Report> {
-  // TODO: check that names are extracted in correct order. Alternatively, consider grouping them with
-  //  their respective data entries into a struct, rather than storing names in a separate array.
-  let names = fastas.iter().map(|fasta| fasta.seq_name.clone()).collect_vec();
+  // // TODO: check that names are extracted in correct order. Alternatively, consider grouping them with
+  // //  their respective data entries into a struct, rather than storing names in a separate array.
+  // let names = fastas.iter().map(|fasta| fasta.seq_name.clone()).collect_vec();
 
   // Build singleton graphs from input sequences
   // TODO: initial graphs can potentially be constructed when initializing tree clades. This could avoid a lot of boilerplate code.
@@ -44,55 +44,58 @@ pub fn build(fastas: Vec<FastaRecord>, args: &PangraphBuildArgs) -> Result<Pangr
     .map(|fasta| Pangraph::singleton(fasta, Strand::Forward, args.circular)) // FIXME: strand hardcoded
     .collect_vec();
 
-  // Calculate pairwise distances between future guide tree nodes
-  let distance = match args.distance_backend {
-    // TODO: this function only needs sequences, and not graphs
-    DistanceBackend::Native => mash_distance(&graphs, &MinimizersParams::default()),
-    DistanceBackend::Mash => {
-      // FIXME: what's the difference between Native and Mash?
-      unimplemented!("DistanceBackend::Mash");
-    }
-  };
+  // FIXME: fix the actual implementation below
+  Ok(graphs[0].clone())
 
-  // Build guide tree
-  let tree = build_tree_using_neighbor_joining(&distance, &names, &graphs)?;
-
-  // Balance guide tree (to increase available parallelism during parallel traversal?)
-  let tree = balance(&tree);
-
-  // Main loop: traverse the tree starting from leaf nodes and build the graphs bottom-up all the way to the root node.
-  // The graph of the root node is the graph we are looking for.
-  postorder(&tree, |clade| {
-    match (&clade.left, &clade.right) {
-      (None, None) => {
-        // Case: leaf node. Action: nothing to do.
-        Ok(())
-      }
-      (Some(left), Some(right)) => {
-        // Case: internal node with two children. Action: produce graph for this node based on the graphs of its children.
-        // Assumption: Child nodes are assumed to be already visited at this point.
-        if let (Some(left), Some(right)) = (&left.read().graph, &right.read().graph) {
-          clade.graph = Some(align_graphs(left, right, args)?);
-          Ok(())
-        } else {
-          make_internal_error!("Found internal clade with two children, of which one or both have no graph attached.")
-        }
-      }
-      (None, Some(child)) | (Some(child), None) => {
-        // Case: internal node with one child. Action: ???
-        unimplemented!("What to do if there's only one child?");
-      }
-    }
-  })
-  .into_iter()
-  .collect::<Result<Vec<_>, Report>>()
-  .wrap_err("When traversing guide tree")?;
-
-  let graph = tree
-    .write()
-    .graph
-    .take()
-    .ok_or_else(|| make_internal_report!("Root clade of the guide tree contains no graph after graph alignment"))?;
-
-  Ok(graph)
+  // // Calculate pairwise distances between future guide tree nodes
+  // let distance = match args.distance_backend {
+  //   // TODO: this function only needs sequences, and not graphs
+  //   DistanceBackend::Native => mash_distance(&graphs, &MinimizersParams::default()),
+  //   DistanceBackend::Mash => {
+  //     // FIXME: what's the difference between Native and Mash?
+  //     unimplemented!("DistanceBackend::Mash");
+  //   }
+  // };
+  //
+  // // Build guide tree
+  // let tree = build_tree_using_neighbor_joining(&distance, &names, &graphs)?;
+  //
+  // // Balance guide tree (to increase available parallelism during parallel traversal?)
+  // let tree = balance(&tree);
+  //
+  // // Main loop: traverse the tree starting from leaf nodes and build the graphs bottom-up all the way to the root node.
+  // // The graph of the root node is the graph we are looking for.
+  // postorder(&tree, |clade| {
+  //   match (&clade.left, &clade.right) {
+  //     (None, None) => {
+  //       // Case: leaf node. Action: nothing to do.
+  //       Ok(())
+  //     }
+  //     (Some(left), Some(right)) => {
+  //       // Case: internal node with two children. Action: produce graph for this node based on the graphs of its children.
+  //       // Assumption: Child nodes are assumed to be already visited at this point.
+  //       if let (Some(left), Some(right)) = (&left.read().graph, &right.read().graph) {
+  //         clade.graph = Some(align_graphs(left, right, args)?);
+  //         Ok(())
+  //       } else {
+  //         make_internal_error!("Found internal clade with two children, of which one or both have no graph attached.")
+  //       }
+  //     }
+  //     (None, Some(child)) | (Some(child), None) => {
+  //       // Case: internal node with one child. Action: ???
+  //       unimplemented!("What to do if there's only one child?");
+  //     }
+  //   }
+  // })
+  // .into_iter()
+  // .collect::<Result<Vec<_>, Report>>()
+  // .wrap_err("When traversing guide tree")?;
+  //
+  // let graph = tree
+  //   .write()
+  //   .graph
+  //   .take()
+  //   .ok_or_else(|| make_internal_report!("Root clade of the guide tree contains no graph after graph alignment"))?;
+  //
+  // Ok(graph)
 }
