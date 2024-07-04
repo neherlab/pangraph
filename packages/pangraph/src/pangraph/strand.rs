646c765d329656dc096a2fc2fe97c1100f5750bd
ivan-aksamentov <ivan.aksamentov@gmail.com>
2024-07-04 07:49:33 +0200

    feat: add strand and orientation methods

--- packages/pangraph/src/pangraph/strand.rs
+++ packages/pangraph/src/pangraph/strand.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use smart_default::SmartDefault;
 
+#[must_use]
 #[derive(Copy, Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Hash, SmartDefault)]
 pub enum Strand {
   #[default]
@@ -8,4 +9,48 @@ pub enum Strand {
   Forward,
   #[serde(rename = "-")]
   Reverse,
+}
+
+impl Strand {
+  pub fn is_forward(&self) -> bool {
+    matches!(self, Strand::Forward)
+  }
+
+  pub fn is_reverse(&self) -> bool {
+    matches!(self, Strand::Reverse)
+  }
+
+  pub fn reverse(&self) -> Strand {
+    match self {
+      Strand::Forward => Strand::Reverse,
+      Strand::Reverse => Strand::Forward,
+    }
+  }
+}
+
+#[must_use]
+#[derive(Copy, Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Hash, SmartDefault)]
+pub enum Orientation {
+  #[default]
+  #[serde(rename = "+")]
+  Forward,
+  #[serde(rename = "-")]
+  Reverse,
+}
+
+impl Orientation {
+  pub fn is_forward(&self) -> bool {
+    matches!(self, Orientation::Forward)
+  }
+
+  pub fn is_reverse(&self) -> bool {
+    matches!(self, Orientation::Reverse)
+  }
+
+  pub fn reverse(&self) -> Orientation {
+    match self {
+      Orientation::Forward => Orientation::Reverse,
+      Orientation::Reverse => Orientation::Forward,
+    }
+  }
 }
