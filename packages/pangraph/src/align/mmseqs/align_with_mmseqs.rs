77b87b6a191c77c0ea97869cf65aa2bd41e9c30f
ivan-aksamentov <ivan.aksamentov@gmail.com>
2024-07-05 07:20:24 +0200

    test: temporarily disable broken tests

--- packages/pangraph/src/align/mmseqs/align_with_mmseqs.rs
+++ packages/pangraph/src/align/mmseqs/align_with_mmseqs.rs
@@ -58,64 +58,69 @@ pub fn align_with_mmseqs(
   Alignment::from_paf_str(&paf_str)
 }
 
-#[cfg(test)]
-mod tests {
-  use super::*;
-  use crate::align::alignment::Hit;
-  use crate::pangraph::strand::Strand;
-  use crate::utils::interval::Interval;
-  use eyre::Report;
-  use maplit::btreemap;
-  use noodles::sam::record::cigar::op::Kind;
-  use noodles::sam::record::cigar::Op;
-  use noodles::sam::record::Cigar;
-  use pretty_assertions::assert_eq;
-  use rstest::rstest;
-
-  #[rstest]
-  fn test_align_with_mmseqs_general_case() -> Result<(), Report> {
-    //                0         1         2         3         4         5         6         7         8         9
-    //                0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
-    let ref_seq = o!("CTTGGAGGTTCCGTGGCTAGATAACAGAACATTCTTGGAATGCTGATCTTTATAAGCTCATGCGACACTTCGCATGGTGAGCCTTTGT");
-    let qry_seq = o!("CTTGGAGGTTCCGTGGCTATAAAGATAACAGAACATTCTTGGAATGCTGATCAAGCTCATGGGACANNNNNCATGGTGGACAGCCTTTGT");
-
-    let params = AlignmentArgs {
-      kmer_length: Some(12),
-      ..AlignmentArgs::default()
-    };
-
-    let blocks = [ref_seq, qry_seq]
-      .into_iter()
-      .enumerate()
-      .map(|(i, seq)| PangraphBlock::new(Some(BlockId(i)), seq.replace(['\n', ' '], ""), btreemap! {}))
-      .map(|block| (block.id(), block))
-      .collect();
-
-    let actual = align_with_mmseqs(&blocks, &params)?;
-
-    let expected = vec![Alignment {
-      qry: Hit::new(BlockId(1), 90, (1, 90)),
-      reff: Hit::new(BlockId(0), 88, (1, 88)),
-      matches: 77,
-      length: 95,
-      quality: 102,
-      orientation: Strand::Forward,
-      new_block_id: None, // FIXME
-      anchor_block: None, // FIXME
-      cigar: Cigar::try_from(vec![
-        Op::new(Kind::Match, 18),
-        Op::new(Kind::Insertion, 4),
-        Op::new(Kind::Match, 30),
-        Op::new(Kind::Deletion, 5),
-        Op::new(Kind::Match, 26),
-        Op::new(Kind::Insertion, 3),
-        Op::new(Kind::Match, 9),
-      ])?,
-      divergence: Some(0.18999999999999995),
-      align: Some(112.0),
-    }];
-
-    assert_eq!(expected, actual);
-    Ok(())
-  }
-}
+// FIXME: This test is failing after commit a62b19b018b4b2f9602bc75335d4ab5ddbc7abf5
+// After that commit, `align_with_mmseqs()` accepts an array of blocks, rather than 2 arrays of ref and query sequences,
+// and all blocks sequences are aligned against ll block sequences. So there is no way to pass refs and qrys separately
+// now - the test needs to be changed. There is also a sibling broken test in `align_with_minimap2`.
+//
+// #[cfg(test)]
+// mod tests {
+//   use super::*;
+//   use crate::align::alignment::Hit;
+//   use crate::pangraph::strand::Strand;
+//   use crate::utils::interval::Interval;
+//   use eyre::Report;
+//   use maplit::btreemap;
+//   use noodles::sam::record::cigar::op::Kind;
+//   use noodles::sam::record::cigar::Op;
+//   use noodles::sam::record::Cigar;
+//   use pretty_assertions::assert_eq;
+//   use rstest::rstest;
+//
+//   #[rstest]
+//   fn test_align_with_mmseqs_general_case() -> Result<(), Report> {
+//     //                0         1         2         3         4         5         6         7         8         9
+//     //                0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+//     let ref_seq = o!("CTTGGAGGTTCCGTGGCTAGATAACAGAACATTCTTGGAATGCTGATCTTTATAAGCTCATGCGACACTTCGCATGGTGAGCCTTTGT");
+//     let qry_seq = o!("CTTGGAGGTTCCGTGGCTATAAAGATAACAGAACATTCTTGGAATGCTGATCAAGCTCATGGGACANNNNNCATGGTGGACAGCCTTTGT");
+//
+//     let params = AlignmentArgs {
+//       kmer_length: Some(12),
+//       ..AlignmentArgs::default()
+//     };
+//
+//     let blocks = [ref_seq, qry_seq]
+//       .into_iter()
+//       .enumerate()
+//       .map(|(i, seq)| PangraphBlock::new(Some(BlockId(i)), seq.replace(['\n', ' '], ""), btreemap! {}))
+//       .map(|block| (block.id(), block))
+//       .collect();
+//
+//     let actual = align_with_mmseqs(&blocks, &params)?;
+//
+//     let expected = vec![Alignment {
+//       qry: Hit::new(BlockId(1), 90, (1, 90)),
+//       reff: Hit::new(BlockId(0), 88, (1, 88)),
+//       matches: 77,
+//       length: 95,
+//       quality: 102,
+//       orientation: Strand::Forward,
+//       new_block_id: None, // FIXME
+//       anchor_block: None, // FIXME
+//       cigar: Cigar::try_from(vec![
+//         Op::new(Kind::Match, 18),
+//         Op::new(Kind::Insertion, 4),
+//         Op::new(Kind::Match, 30),
+//         Op::new(Kind::Deletion, 5),
+//         Op::new(Kind::Match, 26),
+//         Op::new(Kind::Insertion, 3),
+//         Op::new(Kind::Match, 9),
+//       ])?,
+//       divergence: Some(0.18999999999999995),
+//       align: Some(112.0),
+//     }];
+//
+//     assert_eq!(expected, actual);
+//     Ok(())
+//   }
+// }
