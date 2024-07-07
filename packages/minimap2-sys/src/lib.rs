6d2f2285803de07c09f975ed6609c8233d9812c3
ivan-aksamentov <ivan.aksamentov@gmail.com>
2024-07-07 14:08:52 +0200

    chore: silence warnings from generated code

--- packages/minimap2-sys/src/lib.rs
+++ packages/minimap2-sys/src/lib.rs
@@ -1,6 +1,5 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+#![allow(warnings)]
+#![allow(clippy::all)]
 
 #[cfg(feature = "bindgen")]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
