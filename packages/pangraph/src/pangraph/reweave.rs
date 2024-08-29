9619349bcc8c376f9b8092b43c99092582cfe697
Marco Molari <mmolari91@gmail.com>
2024-08-29 16:40:25 +0200

    add checks

--- packages/pangraph/src/pangraph/reweave.rs
+++ packages/pangraph/src/pangraph/reweave.rs
@@ -39,10 +39,32 @@ impl MergePromise {
       .par_iter()
       .map(|(node_id, edits)| {
         let mut seq = edits.apply(self.append_block.consensus())?;
+
+        // debug assert: check that length of sequence is > 0
+        // if not, return an error and print the results for debug
+        debug_assert!(
+          !seq.is_empty(),
+          "Sequence {:?} is empty after applying edits: {:#?}",
+          self.append_block.consensus(),
+          edits
+        );
+
         if !self.orientation.is_forward() {
           seq = reverse_complement(&seq)?;
         };
+        let seq_cpy = seq.clone(); // TODO: for debug check, remove befor merging PR
         let edits = map_variations(self.anchor_block.consensus(), seq)?;
+
+        // check that input and output sequences are the same after edits
+        debug_assert!(
+          seq_cpy == edits.apply(self.anchor_block.consensus())?,
+          "After applying edits, the sequence\n{:?} is not equal to the original input sequence\n{:?}.\nEdits: {:#?}, Anchor block consensus: {:#?}",
+          edits.apply(self.anchor_block.consensus())?,
+          seq_cpy,
+          edits,
+          self.anchor_block.consensus()
+        );
+
         Ok((*node_id, edits))
       })
       .collect::<Result<Vec<_>, Report>>()?
