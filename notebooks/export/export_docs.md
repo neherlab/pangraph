b292fa56e3cac1ef83e90fec39893cf4fd34cf36
Marco Molari <mmolari91@gmail.com>
2024-12-16 14:14:55 +0100

    export block consensus

--- notebooks/export/export_docs.md
+++ notebooks/export/export_docs.md
@@ -24,3 +24,10 @@ Some design decisions that we need to take:
 - we do not align insertions. Exported alignments do not have insertions.
 - what are the id of fasta records for block alignments? Node id (and strain name)?
 - what is the order of blocks in the core-genome alignment? Let the use specify a `guide-strain`.
+
+## blocks to fasta records
+
+Fasta records are currently implemented [here](https://github.com/neherlab/pangraph/blob/rust/packages/pangraph/src/io/fasta.rs).
+
+We can extend the [`apply_edits_to_ref`](https://github.com/neherlab/pangraph/blob/98886771cb20cd4bfe7ce33c52dafc2fc33f6faa/packages/pangraph/src/pangraph/edits.rs#L194) function to output also aligned seqeunces, i.e. sequences without insertions and with gaps at the place of deleted characters.
+In the draft code this is done by either adding an `aligned` binary argument to the function. But adding a new function would work as well.
---
+++ notebooks/export/fasta_utils.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+from .utils import Edit, Block
+
+
+# see: https://github.com/neherlab/pangraph/blob/98886771cb20cd4bfe7ce33c52dafc2fc33f6faa/packages/pangraph/src/pangraph/edits.rs#L194
+# I added the aligned optional argument.
+# When true it will not add insertions and have deletions as gaps
+# if this is not good practice could also be split into two functions,
+# or have an enum for the output type (aligned vs whole sequence)
+def apply_edits_to_ref(edits: Edit, ref: str, aligned=False) -> str:
+    """
+    Apply the edits to the reference to obtain the query sequence.
+    If aligned is True, insertions are not added and deletions are replaced by gaps.
+    """
+    qry = list(ref)
+    for S in edits.subs:
+        qry[S.pos] = S.alt
+
+    for D in edits.dels:
+        for l in range(D.length):
+            qry[D.pos + l] = "-" if aligned else ""
+    if aligned:
+        qry = [q for q in qry if q != "-"]
+        for I in edits.ins:
+            if I.pos > 0:
+                qry[I.pos - 1] += I.ins
+            elif I.pos == 0:
+                qry[0] = I.ins + qry[0]
+    return "".join(qry)
+
+
+# reimplementation of already existing code in pangraph
+# see https://github.com/neherlab/pangraph/blob/rust/packages/pangraph/src/io/fasta.rs
+@dataclass
+class FastaRecord:
+    name: str
+    seq: str
+    idx: int
+
+
+def write_to_file(records: list[FastaRecord], file_path: str):
+    with open(file_path, "w") as f:
+        for record in records:
+            f.write(f">{record.name}\n{record.seq}\n")
+
+
+def block_to_seqrecords(block: Block, aligned=False) -> list[FastaRecord]:
+    records = []
+    for idx, (node_id, edits) in block.alignment.items():
+        seq = apply_edits_to_ref(edits, block.consensus, aligned)
+        records.append(FastaRecord(name=node_id, seq=seq, idx=idx))
+    return records
