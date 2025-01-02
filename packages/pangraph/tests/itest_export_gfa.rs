9fa51f49b146508e22506f5727d858d7f2328f37
ivan-aksamentov <ivan.aksamentov@gmail.com>
2025-01-02 19:37:58 +0100

    test: expand integration tests for export_gfa

--- packages/pangraph/tests/itest_export_gfa.rs
+++ packages/pangraph/tests/itest_export_gfa.rs
@@ -2,7 +2,6 @@ mod common;
 
 #[cfg(test)]
 mod tests {
-
   use eyre::Report;
   use itertools::Itertools;
   use pangraph::commands::export::export_args::PangraphExportGfaArgs;
@@ -11,11 +10,26 @@ mod tests {
   use pangraph::io::gfa::GfaWriteParams;
   use pangraph::pangraph::pangraph::Pangraph;
   use pretty_assertions::assert_eq;
+  use rstest::rstest;
   use std::path::PathBuf;
   use tempfile::tempdir;
 
-  #[test]
-  fn itest_export_gfa() -> Result<(), Report> {
+  #[rustfmt::skip]
+  #[rstest]
+  //      min_length  min_depth   export        n expected
+  //                              duplicated    gfa segments
+  #[case( Some(1000), Some(2),    true,         8          )]
+  #[case( Some(1000), Some(2),    false,        7          )]
+  #[case( None,       None,       true,         14         )]
+  #[case( None,       None,       false,        13         )]
+  //
+  #[trace]
+  fn itest_export_gfa(
+    #[case] minimum_length: Option<usize>,
+    #[case] minimum_depth: Option<usize>,
+    #[case] export_duplicated: bool,
+    #[case] n_expected_gfa_segments: usize,
+  ) -> Result<(), Report> {
     let input_json = Some(PathBuf::from("../../data/test_graph.json"));
     let output = tempdir()?.path().join("output.gfa");
     let graph = Pangraph::from_path(&input_json)?;
@@ -24,12 +38,12 @@ mod tests {
       input_json,
       output: output.clone(),
       params: GfaWriteParams {
-        minimum_length: Some(100),
+        minimum_length,
         maximum_length: None,
-        minimum_depth: None,
+        minimum_depth,
         maximum_depth: None,
         include_sequences: false,
-        no_duplicated: false,
+        no_duplicated: !export_duplicated,
       },
     })?;
 
@@ -37,7 +51,7 @@ mod tests {
     let lines = output.lines().collect_vec();
 
     let segment_lines = lines.iter().filter(|l| l.starts_with("S\t")).collect_vec();
-    assert_eq!(segment_lines.len(), graph.blocks.len());
+    assert_eq!(segment_lines.len(), n_expected_gfa_segments);
 
     Ok(())
   }
