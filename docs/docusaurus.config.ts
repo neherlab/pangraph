97795d1528ed264148ccf39677992ef7cc6552a8
ivan-aksamentov <ivan.aksamentov@gmail.com>
2025-01-13 12:22:28 +0100

    docs: setup math rendering in docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,7 +1,8 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
-
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 const config: Config = {
   title: 'Pangraph docs',
@@ -30,6 +31,8 @@ const config: Config = {
           routeBasePath: '/',
           sidebarPath: './sidebars.ts',
           editUrl: 'https://github.com/neherlab/pangraph/tree/master/docs',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
         },
         blog: false,
         theme: {
