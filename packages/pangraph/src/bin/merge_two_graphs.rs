f052057aaeaf457340b0758190e1f3525637b314
Marco Molari <mmolari91@gmail.com>
2025-04-10 12:31:34 +0200

    feat: verify from the input graphs

--- packages/pangraph/src/bin/merge_two_graphs.rs
+++ packages/pangraph/src/bin/merge_two_graphs.rs
@@ -4,12 +4,12 @@ use eyre::{Report, WrapErr};
 use log::info;
 use pangraph::commands::build::build_args::PangraphBuildArgs;
 use pangraph::commands::reconstruct::reconstruct_run::{compare_sequences, reconstruct};
-use pangraph::io::fasta::read_many_fasta;
 use pangraph::io::json::{JsonPretty, json_write_file};
 use pangraph::pangraph::graph_merging::merge_graphs;
 use pangraph::pangraph::pangraph::Pangraph;
 use pangraph::utils::global_init::global_init;
 use pangraph::utils::global_init::setup_logger;
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 #[ctor]
@@ -31,9 +31,9 @@ struct Args {
   #[clap(value_hint = ValueHint::FilePath)]
   pub right_graph: Option<PathBuf>,
 
+  // binary flag: if set, verify the sequences in the merged graph did not change
   #[clap(long, short = 'V')]
-  #[clap(value_hint = ValueHint::FilePath)]
-  pub verify_seqs: Option<PathBuf>,
+  pub merge_verify: bool,
 }
 
 fn main() -> Result<(), Report> {
@@ -71,21 +71,33 @@ fn main() -> Result<(), Report> {
 
   json_write_file(&args.build_args.output_json, &merged, JsonPretty(true))?;
 
-  // verify
-  if let Some(verify_seqs_path) = args.verify_seqs {
-    info!("Verifying merged graph against {:?}", verify_seqs_path);
-    let mut seqs = reconstruct(&merged);
-    // read sequences into a list
-    let verify_seqs = read_many_fasta(&[verify_seqs_path])?;
+  if args.merge_verify {
+    let initial_left_seqs = reconstruct(&left);
+    let initial_right_seqs = reconstruct(&right);
 
-    seqs.try_for_each(|actual| -> Result<(), Report> {
-      let actual = actual?;
-      // look for sequence with the expected index
-      let idx = actual.index;
-      let expected = &verify_seqs[idx];
-      compare_sequences(&expected, &actual)?;
+    // create a map of {seq.idx -> seq}
+    let mut initial_seqs = HashMap::new();
+    initial_left_seqs
+      .chain(initial_right_seqs)
+      .try_for_each(|seq| -> Result<(), Report> {
+        let seq = seq?;
+        initial_seqs.insert(seq.index, seq);
+        Ok(())
+      })?;
+
+    // verify that the merged graph contains the same sequences
+    let mut merged_seqs = reconstruct(&merged);
+    merged_seqs.try_for_each(|seq| -> Result<(), Report> {
+      let seq = seq?;
+      let idx = seq.index;
+      if let Some(expected) = initial_seqs.get(&idx) {
+        compare_sequences(expected, &seq)?;
+      } else {
+        return Err(eyre::eyre!("Sequence with index {idx} not found in initial sequences").into());
+      }
       Ok(())
     })?;
+    info!("Merged graph contains the same sequences as the original graphs");
   }
 
   Ok(())
